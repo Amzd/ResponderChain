88089f166f0b2d4adb147ab7e839677739d279b7
Casper Zandbergen <info@casperzandbergen.nl>
2021-01-06 19:10:53 +0100

    [SPM] .branch doesn't work with versioning so I switched to a fork

--- Package.swift
+++ Package.swift
@@ -15,9 +15,9 @@ let package = Package(
     ],
     dependencies: [
         .package(name: "Introspect", url: "https://github.com/siteline/SwiftUI-Introspect.git", from: "0.1.2"),
-        .package(url: "https://github.com/MarioIannotta/SwizzleSwift", .branch("master")),
+        .package(url: "https://github.com/Amzd/SwizzleSwift", from: "1.0.0"),
         .package(url: "https://github.com/nalexn/ViewInspector", from: "0.6.0"),
-    ],
+    ],0
     targets: [
         .target(name: "ResponderChain", dependencies: ["Introspect", "SwizzleSwift"]),
         .testTarget(name: "ResponderChainTests", dependencies: ["ResponderChain", "ViewInspector"]),
